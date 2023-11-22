ba514bdc5c599557f1fda8758f33768d6766ae27
sarkabohacova <bohacova.sar@gmail.com>
2023-11-22 12:48:32 +0100

    values exercise

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('funguju!');
+const updateSkill = (id, value) => {
+  const skillBar = document.getElementById(id).querySelector('.skill__progress')
+  const skillValue = document.getElementById(id).querySelector('.skill__value')
+  
+  skillBar.style.width = value + '%'
+  skillValue.textContent = value + ' / 100'
+}
+
+const userValueHTML = prompt("Zadej úroveň dovednosti HTML v čísle 0-100:")
+const updatedHTML = updateSkill('skill1', userValueHTML)
+
+const userValueCSS = prompt("Zadej úroveň dovednosti CSS v čísle 0-100:")
+const updatedCSS = updateSkill('skill2', userValueCSS)
+
+const userValueJS = prompt("Zadej úroveň dovednosti JS v čísle 0-100:")
+const updatedJS = updateSkill('skill3', userValueJS)
